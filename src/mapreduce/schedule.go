3736d73216375fe3b3234c580c33f5e778db8392
Collin Moore <collin.moore96@gmail.com>
2019-02-05 20:55:48 -0500

    first attempt at handling failures without using channels in a non-blocking read style

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -41,38 +41,104 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	// Another new thought: I'm overthinking the concurrency part, they just want me to
 	// use `go` for calling the rpc, so I don't wait on it forever, I think...
 	// okay, I can read from a single channel in multiple go routines, thats good
+	//
+	// Part IV Notes
+	// My first thought is to rework my loop to populate my processChan
+	// I could add a new channel to each of the feeders that they can send
+	// info back to the master on, which can be checked in that loop and added
+	// to the processChan again, probably want to kill that worker feeder, too.
+	// Need to figure out how to check a chan for stuff and continue if empty
+	// I think the right move is to use a select statement
 
 	processChan := make(chan int)
-	var wg sync.WaitGroup
+	workerFailChan := make(chan int)
+	workerSuccessChan := make(chan int, 4) // going to try this as a buffered, so its not blocking the workers
+	var wg sync.WaitGroup                  // for the handleWorker goroutines
 
 	handleWorker := func(rpcaddr string, wg *sync.WaitGroup) {
 		fmt.Println("Handler created for: ", rpcaddr)
 		defer wg.Done()
 		for j := range processChan {
+			fmt.Printf("Starting %sJob %d on worker %s\n", phase, j, rpcaddr)
 			args := DoTaskArgs{jobName, mapFiles[j], phase, j, n_other}
 			result := call(rpcaddr, "Worker.DoTask", args, nil)
 			if !result {
-				break
+				fmt.Printf("%sJob %d failed on worker %s\n", phase, j, rpcaddr)
+				workerFailChan <- j // sending failed task number
+			} else {
+				fmt.Printf("%sJob %d finished on worker %s\n", phase, j, rpcaddr)
 			}
 		}
 		fmt.Println("Handler finished for: ", rpcaddr)
 		return
 	}
 
-	go func(registerChan chan string) {
+	// goroutine to add workers as they register
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
 		for r := range registerChan {
 			wg.Add(1)
 			go handleWorker(r, &wg)
 		}
-	}(registerChan)
+		return
+	}()
 
-	fmt.Printf("Loading up processChan with %d tasks\n", ntasks)
-	for i := 0; i < ntasks; i++ {
-		i := i
-		processChan <- i // could cause a bug if sharing same `i`
-	}
-	close(processChan)
-	fmt.Println("Finished loading processChan")
+	// determine finished
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		completion := make(map[int]bool)
+		count := 0
+		for count < ntasks {
+			fmt.Println("starting success job")
+			job := <-workerSuccessChan
+			fmt.Println("received a success")
+			_, ok := completion[job]
+			if !ok {
+				fmt.Printf("Job %d is complete\n", job)
+				completion[job] = true
+				count++
+			}
+		}
+		fmt.Println("Closing all the channels")
+		close(workerSuccessChan)
+		close(processChan)
+		close(workerFailChan)
+		return
+	}()
+
+	// intial shedule handling
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		for i := 0; i < ntasks; i++ {
+			i := i
+			fmt.Println("loading initial job: ", i)
+			processChan <- i
+		}
+		fmt.Println("finished loading processChan")
+		return
+	}()
+
+	// rescheduling failures
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		for fail := range workerFailChan {
+			fmt.Printf("job %d failed, rescheduling\n", fail)
+			processChan <- fail
+		}
+		return
+	}()
+
+	// fmt.Printf("Loading up processChan with %d tasks\n", ntasks)
+
+	// for i := 0; i < ntasks; i++ {
+	// i := i
+	// processChan <- i // could cause a bug if sharing same `i`
+	// }
+
 	wg.Wait()
 	fmt.Printf("Schedule: %v done\n", phase)
 }
